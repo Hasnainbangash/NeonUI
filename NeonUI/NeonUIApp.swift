39473ea4eefe1c9e89600c7bffd2d7e22902a833
Bangash38 <bangashmhasnain38@gmail.com>
2025-03-01 18:02:53 +0500

    Initial Commit

---
+++ NeonUI/NeonUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeonUIApp.swift
+//  NeonUI
+//
+//  Created by Muhammad Hasnain Bangash on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct NeonUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeonUITests/NeonUITests.swift
@@ -0,0 +1,17 @@
+//
+//  NeonUITests.swift
+//  NeonUITests
+//
+//  Created by Muhammad Hasnain Bangash on 01/03/2025.
+//
+
+import Testing
+@testable import NeonUI
+
+struct NeonUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
